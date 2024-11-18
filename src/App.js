caa93d78b256394c523b160fb00901a18ba92454
AryoYudo <aryoyudho29@gmail.com>
2024-11-18 20:54:05 +0700

    pus About

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './layouts/header.jsx'; 
 import Beranda from './components/Beranda.jsx'; 
+import About from './components/About.jsx'; 
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </div>
       <div className="pt-5 mt-5"> {/* Menambahkan padding-top untuk konten utama */}
         <Beranda />
+      </div>
+      <div className="pt-5 mt-5"> {/* Menambahkan padding-top untuk konten utama */}
+        <About />
       </div>
     </div>
   );
